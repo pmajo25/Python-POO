dbbabd13814ea59ed018ba6bb54c8ecc4293f1b6
pmajo25 <mariajosepaterninalo@gmail.com>
2024-10-09 19:20:19 -0500

    Update Ejercicio-2.py

--- 5. Abstracción/Ejercicio-2.py
+++ 5. Abstracción/Ejercicio-2.py
@@ -5,33 +5,38 @@ Luego, crea dos clases concretas EmpleadoTiempoCompleto y EmpleadoPorHoras que i
 
 from abc import ABC, abstractmethod
 
-# Clase abstracta
+# Clase abstracta Empleado
 class Empleado(ABC):
+    def _init_(self, nombre):
+        self.nombre = nombre
+    
     @abstractmethod
     def calcular_salario(self):
         pass
 
-# Clase concreta para empleados a tiempo completo
+# Clase concreta EmpleadoTiempoCompleto
 class EmpleadoTiempoCompleto(Empleado):
-    def __init__(self, salario_mensual):
+    def __init__(self, nombre, salario_mensual):
+        self.nombre = nombre
         self.salario_mensual = salario_mensual
         
-
     def calcular_salario(self):
         return self.salario_mensual
 
-# Clase concreta para empleados por horas
+# Clase concreta EmpleadoPorHoras
 class EmpleadoPorHoras(Empleado):
-    def __init__(self, horas_trabajadas, pago_por_hora):
+    def __init__(self, nombre, horas_trabajadas, pago_por_hora):
+        self.nombre = nombre
         self.horas_trabajadas = horas_trabajadas
         self.pago_por_hora = pago_por_hora
-       
+    
     def calcular_salario(self):
         return self.horas_trabajadas * self.pago_por_hora
 
 # Uso de las clases
-empleado1 = EmpleadoTiempoCompleto(3000)
-print(f"El salario del empleado a tiempo completo es de: {empleado1.calcular_salario()}")
+empleado_tiempo_completo = EmpleadoTiempoCompleto("Andres", 1000000)
+print(f"Salario de {empleado_tiempo_completo.nombre} es de: {empleado_tiempo_completo.calcular_salario()}")
 
-empleado2 = EmpleadoPorHoras(120, 15)
-print(f"El salario del empleado por horas es de: {empleado2.calcular_salario()}")
+empleado_por_horas = EmpleadoPorHoras("Pablo", 24, 10000)
+print(f"Salario de {empleado_por_horas.nombre} es de: {empleado_por_horas.calcular_salario()}")
+
